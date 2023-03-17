c217e572ee2e7d95aee231a09f28bccb18a685f1
saurabh <rdsaurabh97@gmail.com>
2023-03-18 00:23:49 +0530

    try again

--- src/main/java/com/sts/FirstApplication.java
+++ src/main/java/com/sts/FirstApplication.java
@@ -2,12 +2,19 @@ package com.sts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FirstApplication {
+public class FirstApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstApplication.class, args);
 	}
-
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(FirstApplication.class);
+	}
 }
